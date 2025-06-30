3b2f75ca77860c896ee80eac0f83ae60f54834cf
Punya Alahakoon <78515540+PunyaAlahakoon@users.noreply.github.com>
2025-06-30 16:33:08 +0100

    clean codes

--- 2_kde_cacluations/functions/kde_approx.R
+++ 2_kde_cacluations/functions/kde_approx.R
@@ -2,10 +2,8 @@
 #kde approximation for the ct values 
 # B number of sims 
 
-
 #kde function approximation 
 
-##final version: as of 21/10/2024: use the viral loads of the infected mozzies only. use the same transformation used in analysis2
 kde_approx<-function(B,x,viral_loads,intercept, slope ){
   ct_threshold<-40 
 #  den_estimates <- NULL
@@ -21,16 +19,10 @@ kde_approx<-function(B,x,viral_loads,intercept, slope ){
   
   den_estimates<-data.frame(matrix(NA,ncol=3,nrow=0))
   colnames(den_estimates)<-c("k","x","y")
-  
-  #when k=0, the distribution is the distribution to k=1 multiplied by r to account for the false positives 
- # den_estimates<-rbind(den_estimates,data.frame("k"= rep(0),"ct"=0:40,"fk"=rep(0)))
   for(i in 1:x){
-    #  sample x viral loads and get the sum 
-   # z<-replicate(B,sum(sample(viral_loads,i,replace = F))) #replicate the function B times
     z<-replicate(B,sum(sample(viral_loads,i))) #replicate the function B times 
     
     z=z/x
-  # ct_values<-vl_to_ct(-2.7,36.9,z)
     ct_values<-slope*log10(z)+intercept
    ct_values[ct_values>ct_threshold]=ct_threshold
     #kernsmpooth package:
@@ -40,8 +32,6 @@ kde_approx<-function(B,x,viral_loads,intercept, slope ){
    den_estimates<-rbind(den_estimates,data.frame("k"= rep(i),"x"=kernel_i$x,"y"=kernel_i$y))
   }
     
-  #den_estimates$fk[den_estimates$fk<0]=0.01
- #den_estimates[1,3]= den_estimates[1,3]*r
   
   return(all_kdes)
 }
